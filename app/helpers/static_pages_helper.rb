4f6d625198a596675a151bf4f86d1b0f31a2ce2b
xiaoyi2013 <yiding2020@gmail.com>
2013-04-17 00:48:12 +0800

    implement rspec test

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,10 +1,2 @@
 module StaticPagesHelper
-  def full_title(page_title)
-    base_title = "Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
 end
